1d447bd241dc43efc1bc33d3f7dd8a58ea712ca1
trade-2990wx <na>
2021-10-20 07:46:37 -0400

    added GetRealTypeName() ext

--- CustomShared/ReflectionUtils.cs
+++ CustomShared/ReflectionUtils.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Text;
 using CustomShared.CustomAttributes;
 using log4net;
 
@@ -168,6 +169,25 @@ namespace CustomShared
         public static TAttr GetAttribute<TAttr>(this PropertyInfo propertyInfo) where TAttr : Attribute
         {
             return (TAttr)propertyInfo.GetCustomAttribute(typeof(TAttr));
+        }
+        
+        public static string GetRealTypeName(this Type t)
+        {
+            if (!t.IsGenericType)
+                return t.Name;
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append(t.Name.Substring(0, t.Name.IndexOf('`')));
+            sb.Append('<');
+            bool appendComma = false;
+            foreach (Type arg in t.GetGenericArguments())
+            {
+                if (appendComma) sb.Append(',');
+                sb.Append(GetRealTypeName(arg));
+                appendComma = true;
+            }
+            sb.Append('>');
+            return sb.ToString();
         }
     }
 }
