f3038451c22d50c63ea2f593feb2c79263cb0182
nren xps 17 <nrenzoni1@gmail.com>
2023-01-06 15:29:16 -0500

    add proto decimal converters

--- GrpcShared/GrpcCommonConverters.cs
+++ GrpcShared/GrpcCommonConverters.cs
@@ -1,9 +1,69 @@
+using CommonProto;
+using CustomShared;
+using DecimalWithInf = CustomShared.DecimalWithInf;
 using LocalDate = NodaTime.LocalDate;
 
 namespace GrpcShared;
 
 public static class GrpcCommonConverters
 {
+    public static CommonProto.Decimal Convert(
+        decimal inDecimal)
+    {
+        var decimalRet = new CommonProto.Decimal();
+        decimalRet.Whole = inDecimal.WholePart();
+        var (decimalRetFraction, shiftCount) = inDecimal.FractionalPart();
+        decimalRet.Fraction = decimalRetFraction;
+        decimalRet.FractionShiftLeft = shiftCount;
+        return decimalRet;
+    }
+
+    public static decimal Convert(
+        CommonProto.Decimal inDecimal)
+    {
+        return MathUtils.DecimalFromParts(
+            inDecimal.Whole,
+            inDecimal.Fraction,
+            inDecimal.FractionShiftLeft);
+    }
+
+    public static CommonProto.DecimalWithInf Convert(
+        DecimalWithInf inDec)
+    {
+        var decRet = new CommonProto.DecimalWithInf();
+        if (inDec.Value is null)
+        {
+            var inf = new CommonProto.Inf
+            {
+                NegativeInf = inDec.NegativeInfinity
+            };
+            decRet.Inf = inf;
+        }
+        else
+        {
+            decRet.Value = Convert(inDec.Value.Value);
+        }
+
+        return decRet;
+    }
+
+    public static DecimalWithInf Convert(
+        CommonProto.DecimalWithInf inDecimalWithInf)
+    {
+        switch (inDecimalWithInf.KindCase)
+        {
+            case CommonProto.DecimalWithInf.KindOneofCase.Value:
+                return new DecimalWithInf(Convert(inDecimalWithInf.Value));
+            case CommonProto.DecimalWithInf.KindOneofCase.Inf:
+                var negInf = inDecimalWithInf.Inf.NegativeInf;
+                return new DecimalWithInf(
+                    !negInf,
+                    negInf);
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+
     public static LocalDate Convert(
         CommonProto.LocalDate inLocalDate)
     {
@@ -23,4 +83,7 @@ public static class GrpcCommonConverters
             Date = (uint)inLocalDate.Day
         };
     }
+
+    public static (string, decimal) Convert(KeyValueDecimal inKeyValueDecimal)
+        => (inKeyValueDecimal.Key, Convert(inKeyValueDecimal.Value));
 }
